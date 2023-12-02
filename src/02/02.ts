12c6f124ba73f10ad7e4b5a7b7a0983ac4fbd1ed
Joep Joosten <joep.joosten@ihomer.nl>
2023-12-02 12:14:14 +0100

    Rename

--- src/02/02.ts
+++ src/02/02.ts
@@ -7,8 +7,9 @@ import { spaces1, string, int,  } from "parser-ts/string";
 import { chain, Parser, bindTo, bind, sepBy } from "parser-ts/Parser";
 import { parseInput } from "@/utils";
 
-type Set = Array<{ count: number, color: string }>;
-type Game = { game: number, sets: Array<Set> };
+type Cube = { count: number, color: string };
+type Cubes = Array<Cube>;
+type Game = { game: number, sets: Array<Cubes> };
 
 const gameNumber = pipe(string("Game"), chain(() => spaces1), chain(() => int));
 const cube = pipe(spaces1, bind('count', () => int), bind('color', () => pipe(spaces1, chain(() => many1(lower)))));
@@ -29,7 +30,7 @@ export const solve2 = (input: ReturnType<typeof parse>) => pipe(
   input,
   RA.map(({ sets }) => A.flatten(sets)),
   RA.map(flow(
-    concatHands,
+    maxCountPerCube,
     R.toEntries,
     RA.map(([, v]) => v),
     RA.reduce(1, (a, b) => a * b))
@@ -37,7 +38,7 @@ export const solve2 = (input: ReturnType<typeof parse>) => pipe(
   RA.reduce(0, (a, b) => a + b)
 )
 
-const concatHands = (hands: Set) => hands.reduce((acc, { count, color }) => ({...acc, [color]: Math.max(acc[color] ?? 0, count)}), {} as Record<string, number>)
+const maxCountPerCube = (cubes: Cubes) => cubes.reduce((acc, { count, color }) => ({...acc, [color]: Math.max(acc[color] ?? 0, count)}), {} as Record<string, number>)
 
 export function partOne(input: ReturnType<typeof parse>) {
   return solve({red: 12, green: 13, blue: 14 })(input)
