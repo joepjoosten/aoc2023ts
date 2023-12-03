cbb325d04847818b2afc700bccc373b2abc11744
Joep Joosten <joep.joosten@ihomer.nl>
2023-12-03 08:05:49 +0100

    cleanup

--- src/03/03.test.ts
+++ src/03/03.test.ts
@@ -6,11 +6,11 @@ const { default: example } = await import(`@/03/example.txt`)
 describe('Day 3', () => {
   describe('Part One', () => {
     it('should work', () => {
-      const input =parse(example);
-      console.log(input);
-      console.log(partOne(input));
+      const input = parse(example)
+      console.log(input)
+      console.log(partOne(input))
     })
   })
-  
+
   describe('Part Two', () => {})
 })
