34893567efaf7f70c3d09019ef7968b73d236d8f
Joep Joosten <joep.joosten@ihomer.nl>
2023-12-03 08:07:14 +0100

    formatting

--- src/03/03.ts
+++ src/03/03.ts
@@ -18,16 +18,7 @@ export function parse(input: string) {
       case '.':
         partNumber = undefined
         break;
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
+      case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
         if (partNumber === undefined) {
           let isDigit = true
           let pos = 1
